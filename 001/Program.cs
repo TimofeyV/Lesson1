0d075dcaa03f91a388a6a9cc59a9e795581b43c4
TimofeyV <vazhenin.t@gmail.com>
2023-04-09 00:40:41 +0300

    work 1

---
+++ 001/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число A: ");
+string text1 = Console.ReadLine();
+int number1 = Convert.ToInt32(text1);
+Console.Write("Введите число B: ");
+string text2 = Console.ReadLine();
+int number2 = Convert.ToInt32(text2);
+
+if (number1 > number2)
+{
+    Console.WriteLine("Число А больше, чем B");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine("Число B больше, чем A");
+}
+else
+{
+    Console.WriteLine("Числа A и B равны");  
+}
