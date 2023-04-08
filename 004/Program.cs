82efd042af3b404a440a779833e1c01c594b35ce
TimofeyV <vazhenin.t@gmail.com>
2023-04-09 01:10:57 +0300

    work 4

---
+++ 004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+string text1 = Console.ReadLine();
+int number1 = Convert.ToInt32(text1);
+
+int count = 2;
+while (count <= number1)
+{
+    Console.Write(count);
+    Console.Write(" ");
+    count += 2;
+}
