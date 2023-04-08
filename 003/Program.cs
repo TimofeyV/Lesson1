18a5aa88d881325079dbd4b4a26c3c02233cded2
TimofeyV <vazhenin.t@gmail.com>
2023-04-09 01:01:46 +0300

    work 3

---
+++ 003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+string text1 = Console.ReadLine();
+int number1 = Convert.ToInt32(text1);
+
+if (number1 % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечётное");
+}
